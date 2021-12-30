9c04877c00456d62cb064533a84f5cfbb04b979f
igor-melo <i90rmc@gmail.com>
2021-12-30 10:06:51 -0300

    Projeto criado

---
+++ src/main/java/com/udemy/workshopspringh2/Workshopspringh2Application.java
@@ -0,0 +1,13 @@
+package com.udemy.workshopspringh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopspringh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopspringh2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/workshopspringh2/Workshopspringh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.workshopspringh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopspringh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
